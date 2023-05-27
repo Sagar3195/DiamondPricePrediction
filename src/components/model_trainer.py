d2db2cc4c88b1ff6cf2c503d0113c0424b8b9625
Sagar3195 <saggyd95@gmail.com>
2023-05-27 13:56:58 +0530

    Model Training

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -3,3 +3,66 @@ import sys
 import pickle
 import numpy as np
 import pandas as pd
+from src.exception import CustomException 
+from src.logger import logging 
+from sklearn.linear_model import LinearRegression, Ridge, Lasso, ElasticNet
+from sklearn.tree import DecisionTreeRegressor
+from src.utils import save_object, evaluate_model
+from dataclasses import dataclass
+
+@dataclass
+class ModelTrainerConfig:
+    train_model_file_path= os.path.join('artifacts', 'model.pkl')
+
+class ModelTrainer:
+    def __init__(self):
+        self.model_trainer_config= ModelTrainerConfig()
+
+    def initiate_model_training(self, train_array, test_array):
+        try:
+            logging.info("Splitting Indepenedent an dependent variable from train and test dataset.")
+            X_train, y_train, X_test, y_test= (
+                train_array[:,:-1],
+                train_array[:,-1],
+                test_array[:,:-1],
+                test_array[:,-1]
+
+            )
+
+            #Train the multiple models
+            models= {
+                "Linear Regression": LinearRegression(),
+                "Lasso": Lasso(),
+                "Ridge": Ridge(),
+                "ElasticNet": ElasticNet(),
+                "DecisionTreeRegression": DecisionTreeRegressor()
+
+            }
+            
+            model_report:dict= evaluate_model(X_train, y_train, X_test, y_test,models)
+            print(model_report)
+            print("\n=\n"*5)
+            logging.info(f"Model Report: {model_report}")
+
+            #To get best model score from dictionary 
+            best_model_score= max(sorted(model_report.values()))
+
+            best_model_name= list(model_report.keys())[list(model_report.values()).index(best_model_score)]
+
+            best_model= models[best_model_name]
+
+            print(f"Best Model Found, Model Name: {best_model_name}, R2 Score: {best_model_score}")
+            print("\n=\n"*5)
+            logging.info(f"Best Model Found, Model Name: {best_model_name}, R2 Score: {best_model_score}")
+
+            save_object(
+                file_path= self.model_trainer_config.train_model_file_path,
+                obj= best_model
+            )
+
+
+
+        except Exception as e:
+            logging.info("Exception occured at Model Training")
+            raise CustomException(e, sys)
+
--- src/pipelines/training_pipeline.py
+++ src/pipelines/training_pipeline.py
@@ -8,6 +8,7 @@ from dataclasses import dataclass
 
 from src.components.data_ingestion import DataIngestion
 from src.components.data_transformation import DataTransformation
+from src.components.model_trainer import ModelTrainer 
 
 
 
@@ -16,6 +17,7 @@ if __name__=='__main__':
     train_data_path, test_data_path= obj.initiate_data_ingestion()
     print(train_data_path, test_data_path)
     data_transformation= DataTransformation()
-    data_transformation.initiate_data_transformation(train_data_path, test_data_path)
+    train_arr, test_arr,_= data_transformation.initiate_data_transformation(train_data_path, test_data_path)
+    model_trainer= ModelTrainer()
+    model_trainer.initiate_model_training(train_arr, test_arr)
     
-     
